a38eaed94388b356615f898a2f7a5dcc60074276
Juan Becerra <jdb9056@nyu.edu>
2024-04-05 18:24:41 -0400

    Commit Dynamic Array Implementation
    This commit introduces a basic implementation of a dynamic array. For
    improved flexibility, this data structure is type agnostic. To account
    for any type, the size in bytes of the type must be specified when
    initializing the array in order to properly manage memory allocations
    and access the data.
    
    This data structure was implemented to simplify storing the tokenized
    command line input and provide greater flexibility when parsing the
    input.

--- shell.c
+++ shell.c
@@ -80,3 +80,101 @@ void _PrintError(const char *func, int line, const char *format, ...) {
   
   va_end(args);
 }
+
+/**
+ * @brief Initializes a dynamic array with specified size and type size.
+ *
+ * Failure to allocate the data in the dynamic array results in freeing up
+ * the memory allocated for the `DynamicArray` struct.
+ *
+ * @param size      Initial number of elements the array can hold.
+ * @param type_size Size of each element in the array.
+ *
+ * @return Pointer to the initialized dynamic array if sucesseful. Otherwise,
+ *         returns NULL if memory allocation fails at any point and `errno` is
+ *         set appropriately.
+ *
+ * @note Allocated dynamic array can be freed using `FreeDynamicArray()`.
+ */
+DynamicArray* InitDynamicArray(size_t size, size_t type_size) {
+  DynamicArray* da = malloc(sizeof(DynamicArray));
+  if (!da) {
+    return NULL;
+  }
+
+  da->data = malloc(size * type_size);
+  if (!da->data) {
+    FreeDynamicArray(da);
+    return NULL;
+  }
+  da->size = size;
+  da->len = 0;
+  da->type_size = type_size;
+
+  return da;
+}
+
+/**
+ * @brief Frees the memory allocated for a dynamic array.
+ *
+ * Dynamic array pointers set to NULL are ignored.
+ *
+ * @param da Pointer to the dynamic array to be freed.
+ */
+void FreeDynamicArray(DynamicArray* da) {
+  if (da) {
+    if (da->data) {
+      free(da->data);
+    }
+    free(da);
+  }
+}
+
+/**
+ * @brief Appends an element to the dynamic array.
+ *
+ * If the array is already full, it attempts to resize the array to double its
+ * current size.
+ *
+ * @param da   Pointer to the dynamic array where the element will be added.
+ * @param elem Pointer to the element to be added to the dynamic array.
+ *
+ * @return 0 if the element is successfully added. Otherwise, returns -1 if
+ *         dynamic array cannot be resized and `errno` is set appropriately.
+ */
+int AppendElement(DynamicArray* da, void* elem) {
+  if (da->size == da->len) {
+    if (!ResizeDynamicArray(da, da->size * 2)) {
+      return -1;
+    }
+    da->size *= 2;
+  }
+  // Using char* enables copying byte-to-byte
+  memcpy((char*)da->data + (da->len * da->type_size), elem, da->type_size);
+  da->len++;
+
+  return 0;
+}
+
+/**
+ * @brief Resizes a dynamic array to a new size.
+ *
+ * If resizing is successful, updates the array's size and data pointer to the
+ * reallocated memory block.
+ *
+ * @param da       Pointer to the dynamic array to be resized.
+ * @param new_size The new size for the dynamic array.
+ *
+ * @return 0 if the dynamic array was successfully resized. On error, -1 is
+ *         returned and `errno` is set appropriately,
+ */
+int ResizeDynamicArray(DynamicArray* da, size_t new_size) {
+  void* resized_data = reallocarray(da->data, new_size, da->type_size);
+  if (!resized_data) {
+    return -1;
+  }
+  da->data = resized_data;
+  da->size = new_size;
+
+  return 0;
+}
--- shell.h
+++ shell.h
@@ -12,13 +12,29 @@
 
 #define PrintError(format, ...) _PrintError(__func__, __LINE__, format, ##__VA_ARGS__)
 
+typedef struct {
+  void* data;
+  size_t size;
+  size_t len;
+  size_t type_size;
+} DynamicArray;
+
 const size_t kInputMax = 1024;
 const size_t kPathMax = 512;
 const char *kPromptString = "\\u@\\h : \\b\n";
 const size_t kHostnameMax = 64;
 const unsigned int kRootUID = 0;
 
+// Shell Functions
 void ExpandPromptString(void);
+
+// Dynamic Array Methods
+int AppendElement(DynamicArray* da, void* elem);
+void FreeDynamicArray(DynamicArray* da);
+DynamicArray* InitDynamicArray(size_t size, size_t type_size);
+int ResizeDynamicArray(DynamicArray* da, size_t new_size);
+
+// Utility Functions
 void _PrintError(const char *func, int line, const char *format, ...);
 
 #endif  // SHELL_H_
